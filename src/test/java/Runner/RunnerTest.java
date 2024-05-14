5e5394ea5199301edb3942aba9f86d63738fd896
Ajit Kumar (QA) <ajit.kumar2@zenesys.com>
2024-05-14 15:18:06 +0530

    BaseClassCode

--- src/test/java/Runner/RunnerTest.java
+++ src/test/java/Runner/RunnerTest.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/Features", tags="@login",glue = {"StepDefinition"},
+@CucumberOptions(features="src/test/java/Features", tags="@login",glue = {"StepDefinition","base"},
 plugin = {"pretty", "html:target/HTML-reports/test.html","json:target/cucumber-report/cucumber.json"})
 
 
